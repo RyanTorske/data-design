d7d4219f6c1219b40d61e0951bae27e229f8cd5b
Ryan Torske <rtorske@cnm.edu>
2020-01-15 19:18:08 -0700

    Added ERD conceptual model

--- epic/entity-relationship-diagram.php
+++ epic/entity-relationship-diagram.php
@@ -1,30 +1,33 @@
 <Title>Entities & Attributes</Title>
 <h1><strong>Profile</strong></h1>
 <ul>
-	<li>youtubeProfileId(primary key)</li>
-	<li>youtubeUserName(primary key)</li>
-	<li>youtubeEmail(primary key)</li>
+	<li>ProfileId(primary key)</li>
+	<li>profileUserName(primary key)</li>
+	<li>profileEmail(primary key)</li>
+	<li>profileInfo (primary Key)</li>
+	<li>profileDateJoined (primary key)</li>
 </ul>
 
-<h2><strong>Videos</strong></h2>
+<h2><strong>Video</strong></h2>
 <ul>
-	<li>userSubscribe(primary key)</li>
-	<li>userWatchLater(foreign key)</li>
-	<li>userLike(foreign key)</li>
-	<li>userDislike(foreign key)</li>
+	<li>videoId(primary key)</li>
+	<li>videoLength(foreign key)</li>
+	<li>videoDate(foreign key)</li>
+	<li>videoViews(foreign key)</li>
+	<li>videoTags (foreign key)</li>
 </ul>
 
-<h3><strong>Profiles</strong></h3>
+<h3><strong>Comment</strong></h3>
 <ul>
-	<li>userBlock(primary key)</li>
-	<li>userMessage(foreign key)</li>
-	<li>userCommentRemove(foreign key)</li>
+	<li>commentId(primary key)</li>
+	<li>commentDate(foreign key)</li>
+	<li>commentContent(foreign key)</li>
 </ul>
 
 <h4>Relations</h4>
 <ul>
 	<li>One User Profile can write one comment per channel-(1 to n)</li>
-	<li>Many videos can be liked by many people (m to n)</li>
+	<li>One profile can like many videos -(1 to n)</li>
 </ul>
 
 
