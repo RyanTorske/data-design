55c6f97c38a500797653f7e3310a7082b26466d2
Ryan Torske <rtorske@cnm.edu>
2020-01-22 16:07:07 -0700

    Finalized OML. Ran and no errors.

--- sql/oml.sql
+++ sql/oml.sql
@@ -1,11 +1,16 @@
-INSERT INTO profile (profileId, profileEmail, profileDescription, profileUsername, profileDateJoined) VALUES
+INSERT INTO profile(profileId, profileEmail, profileDescription, profileUsername, profileDateJoined) VALUES
 (UNHEX('3ed2910c4fcc49d9a90e569e50043e9d'), 'tylerok@yahoo.com', 'Basic channel to watch and view youtube videos', 'Tyler The Creator', NOW());
 
-INSERT INTO video (videoId, videoProfileId, videoDate, videoDescription) VALUES
+INSERT INTO video(videoId, videoProfileId, videoDate, videoDescription) VALUES
 (UNHEX('88a6dc5fb1324144be251e74787910e1'), (UNHEX('3ed2910c4fcc49d9a90e569e50043e9d')), NOW(), 'blablablablablabla');
 
-INSERT INTO comment (commentProfileId, commentVideoId, commentDate, commentContent) VALUES
-(UNHEX('3ed2910c4fcc49d9a90e569e50043e9d'), UNHEX('88a6dc5fb1324144be251e74787910e1'), NOW(), 'This video is amazing and well produced.');
+INSERT INTO comment(commentProfileId, commentVideoId, commentDate, commentContent) VALUES
+(UNHEX('3ed2910c4fcc49d9a90e569e50043e9d'), (UNHEX('88a6dc5fb1324144be251e74787910e1')), NOW(), 'This video is amazing and well produced.');
 
-UPDATE profile SET profileDescription= 'I am a lyrical genius and like to upload my talent.' WHERE profileId= UNHEX('3ed2910c4fcc49d9a90e569e50043e9d');
+UPDATE profile SET profileDescription = 'I am a lyrical genius and like to upload my talent.' WHERE profileId = (UNHEX('3ed2910c4fcc49d9a90e569e50043e9d'));
 
+SELECT videoId, videoProfileId, videoDate, videoDescription FROM video WHERE videoProfileId= (UNHEX('3ed2910c4fcc49d9a90e569e50043e9d'));
+
+SELECT video.videoId, profile.profileId FROM video INNER JOIN profile on video.videoProfileId = profile.profileId WHERE videoDescription = 'blablablablablabla';
+
+DELETE FROM video WHERE videoProfileId = (UNHEX('88a6dc5fb1324144be251e74787910e1'));
